1b36c2967a398116fc93301ee9b7136146966038
Matthew Leahy <matthew.leahy@wearenova.co.uk>
2020-09-14 11:00:57 +0100

    search box added to initial homepage

--- entertainment-app/src/App.js
+++ entertainment-app/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <h1>Entertainment Database</h1>
+      </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
---
+++ entertainment-app/src/components/Search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <section className="searchbox-wrap">
+      <input type="text" placeholder="Movie search..." className="searchbox" />
+    </section>
+  );
+}
